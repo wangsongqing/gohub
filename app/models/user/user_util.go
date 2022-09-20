f7274dcfe7633d262c01ddf4010b6c36f807975a
wangsongqing <xmwme@qq.com>
2022-09-20 11:52:17 +0800

    验证分页请求

--- app/models/user/user_util.go
+++ app/models/user/user_util.go
@@ -1,7 +1,10 @@
 package user
 
 import (
+	"github.com/gin-gonic/gin"
+	"gohub/pkg/app"
 	"gohub/pkg/database"
+	"gohub/pkg/paginator"
 )
 
 // IsEmailExist 判断 Email 已被注册
@@ -46,5 +49,17 @@ func Get(id string) (userModel User) {
 // All 获取所有用户数据
 func All() (users []User) {
 	database.DB.Find(&users)
+	return
+}
+
+// Paginate 分页内容
+func Paginate(c *gin.Context, perPage int) (users []User, paging paginator.Paging) {
+	paging = paginator.Paginate(
+		c,
+		database.DB.Model(User{}),
+		&users,
+		app.V1URL(database.TableName(&User{})),
+		perPage,
+	)
 	return
 }
